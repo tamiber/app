43febb73e648fef8d9aa12c9693def2a5ea2483b
Tamires Bergamim <tamiber@msn.com>
2020-12-02 18:23:09 +0000

    new version

---
+++ script.js
@@ -0,0 +1,15 @@
+let hora = 20;
+
+if(hora > 6 && hora < 12){
+
+    console.log("Bom dia");
+
+}
+else if (hora > 12 && hora< 18){
+    console.log("Boa Tarde");
+
+} else {
+
+    console.log("Boa noite");
+
+}
